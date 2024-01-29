de32336d868e96f5eb5342c3069485069cff62e9
SesH-iO <sesh.io1022@gmail.com>
2024-01-29 13:53:29 +0530

    Building the hero section of the landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 // MERGE CLASSNAMES
 import {cn} from "../lib/utils";
+import Navbar from "../components/Navbar";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -21,6 +22,7 @@ export default function RootLayout({
 		<html lang="en" className="h-full">
 			<body className={cn("relative h-full font-sans antialiased", inter.className)}>
 				<main className="relative flex flex-col min-h-screen">
+					<Navbar /> 
 					<div className="flex-grow flex-1">{children}</div>
 				</main>
 			</body>
