d80d2f13a78273d563c84705b9a7c323806d3ebf
SesH-iO <sesh.io1022@gmail.com>
2024-02-27 20:55:21 +0530

    AirBnB inspired image slider for products

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -24,7 +24,7 @@ const ProductReel = (props: ProductReelTypes) => {
 			query,
 		},
 		{
-			getNextPageParam: (lastPage) => lastPage.nextPage,
+			getNextPageParam: ({nextPage}) => nextPage,
 		}
 	);
 
@@ -62,7 +62,7 @@ const ProductReel = (props: ProductReelTypes) => {
 				<div className="mt-6 flex items-center w-full">
 					<div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8">
 						{/* SHOW PRODUCTS HERE */}
-						{map.map((product, i) => (
+						{map?.map((product, i) => (
 							<ProductListing product={product} index={i} key={i} />
 						))}
 					</div>
